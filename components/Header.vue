a36402819a39ca32fc56be988437ce929f51ca41
sukebon <mukaibook@gmail.com>
2020-11-11 22:48:38 +0900

    add hero.vue

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+header.header.w-full
+  .header__inner.container.mx-auto.flex.justify-between.items-center
+    h1.header__logo
+      svgLogo
+    nav.header__nav
+      ul.header__ul.flex
+        li.header__li.ml-2(
+          v-for="(category, index) in categorys",
+          :key="index"
+        ) {{ category }}
+</template>
+<script>
+import svgLogo from "../assets/logo.svg";
+export default {
+  components: {
+    svgLogo,
+  },
+  data() {
+    return {
+      categorys: ["Home", "About", "Contact"],
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+.header {
+  &__inner {
+    width: 100%;
+    height: 100px;
+    // @media screen and (min-width: 768px) {
+    //   width: 1000px;
+    // }
+  }
+}
+svg {
+  width: 200px;
+}
+</style>
---
+++ components/Hero.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.hero.w-full.mt-5
+  .hero__inner.container.mx-auto.flex.justify-end
+    //- .hero__content
+    //-   .hero__title
+    //-   .hero__disc
+    .hero__wrap
+      .hero__cover
+      .hero__img
+        img(src="~assets/foodfactory.jpg")
+</template>
+<script>
+export default {
+  methods: {
+    slideCover(cover, img) {
+      let tm = new TimelineMax({
+        scrollTringger: {
+          trigger: img,
+          start: "top top",
+        },
+      });
+      tm.to(cover, 0.5, {
+        delay: 1,
+        top: 0,
+        bottom: 0,
+        ease: Power4.easeIn,
+      });
+      tm.to(cover, 0.7, { top: 0, bottom: "100%", ease: Power4.easeIn });
+      tm.to(img, 0.7, { opacity: 1 }, "-=0.5");
+    },
+  },
+  mounted() {
+    this.slideCover(".hero__cover", ".hero__img img");
+  },
+};
+</script>
+<style scoped lang="scss">
+.hero {
+  &__wrap {
+    position: relative;
+  }
+  &__cover {
+    position: absolute;
+    width: 100%;
+    top: 100%;
+    bottom: 0;
+    background-color: #2979dc;
+    z-index: 1;
+  }
+  & img {
+    opacity: 0;
+  }
+}
+</style>
